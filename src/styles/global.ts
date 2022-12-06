9d03caba1f25dd40ce8d31542b49d4d34d7e77fd
vhasousa <vitorhugo_as@hotmail.com>
2022-12-06 13:28:52 -0300

    Create header component

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,6 +13,10 @@ export const GlobalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
   }
 
+  a {
+    text-decoration: none;
+  }
+
   body, input, textarea, button {
     font-family: 'Roboto', sans-serif;
     font-weight: 400;
