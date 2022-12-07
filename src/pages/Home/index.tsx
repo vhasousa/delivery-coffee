5dede3341707d2fc9dc0b229e066b561cf98b071
vhasousa <vitorhugo_as@hotmail.com>
2022-12-06 23:12:59 -0300

    Add coffee card to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,28 @@
+import { Card } from '../../components/Card'
+import { Hero } from './Hero'
 import { HomeContainer } from './styles'
 
 export function Home() {
-  return <HomeContainer>Home</HomeContainer>
+  return (
+    <HomeContainer>
+      <Hero />
+      <section className="coffees">
+        <h2>Nossos cafés</h2>
+        <div className="coffee-cards">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </section>
+    </HomeContainer>
+  )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,25 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-export const HomeContainer = styled.main``
+export const HomeContainer = styled.main`
+  .coffees {
+    max-width: 1200px;
+    display: flex;
+    flex-direction: column;
+    margin: 4rem auto;
+
+    h2 {
+      font-size: 2rem;
+      font-weight: 800;
+      font-family: 'Baloo 2';
+      color: ${(props) => props.theme['base-title']};
+    }
+  }
+
+  .coffee-cards {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    margin-top: 3rem;
+    gap: 3rem;
+  }
+`
