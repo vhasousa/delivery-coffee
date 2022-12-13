d158b5d22f38c90af2e1532c2bc66f7319b504a4
vhasousa <vitorhugo_as@hotmail.com>
2022-12-13 17:45:40 -0300

    Payment method selection

--- src/pages/Checkout/components/PaymentMethod/index.tsx
+++ src/pages/Checkout/components/PaymentMethod/index.tsx
@@ -1,7 +1,15 @@
 import { Bank, CreditCard, CurrencyDollar, Money } from 'phosphor-react'
+import { useContext } from 'react'
+import { OrderContext } from '../../../../contexts/OrderContext'
 import { PaymentMethodContainer } from './styles'
 
 export function PaymentMethod() {
+  const { handleSelectedPaymentMethod } = useContext(OrderContext)
+
+  function handleSelectedMethod(paymentMethod: string) {
+    handleSelectedPaymentMethod(paymentMethod)
+  }
+
   return (
     <PaymentMethodContainer>
       <div className="payment__title">
@@ -12,15 +20,15 @@ export function PaymentMethod() {
         </div>
       </div>
       <div className="payment__options">
-        <button>
+        <button onClick={() => handleSelectedMethod('creditCard')}>
           <CreditCard size={20} />
           <span>CARTÃO DE CRÉDITO</span>
         </button>
-        <button>
+        <button onClick={() => handleSelectedMethod('debitCard')}>
           <Bank size={20} />
           <span>CARTÃO DE DÉBITO</span>
         </button>
-        <button>
+        <button onClick={() => handleSelectedMethod('money')}>
           <Money size={20} />
           <span>DINHEIRO</span>
         </button>
