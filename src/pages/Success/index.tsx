1e46ed04c37c581f410c4f8cee5d7abc365d1e3b
vhasousa <vitorhugo_as@hotmail.com>
2022-12-15 16:27:55 -0300

    Style and functionality of success page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,5 +1,79 @@
-import { SuccessContainer } from './styles'
+import { CurrencyDollar, MapPin, Money, Timer } from 'phosphor-react'
+import { SuccessContainer, SuccessInfo, SuccessItem } from './styles'
+import deliveryIllustration from '../../assets/illustration.png'
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import { OrderData } from '../../contexts/OrderContext'
 
 export function Success() {
-  return <SuccessContainer>Sucesso</SuccessContainer>
+  const { state } = useLocation()
+  const { paymentMethod, address } = state as OrderData
+
+  const [paymentMethodTranslate, setPaymentMethodTranslate] = useState('')
+
+  function handleTranslatePaymentMethod() {
+    switch (paymentMethod) {
+      case 'credit':
+        setPaymentMethodTranslate('Cartão de crédito')
+        break
+      case 'debit':
+        setPaymentMethodTranslate('Cartão de débito')
+        break
+      case 'cash':
+        setPaymentMethodTranslate('Dinheiro')
+        break
+      default:
+        return paymentMethodTranslate
+    }
+  }
+
+  useEffect(() => {
+    handleTranslatePaymentMethod()
+  }, [])
+
+  return (
+    <SuccessContainer>
+      <div>
+        <div className="success__title">
+          <h1>Uhu! Pedido confirmado</h1>
+          <p>Agora é só aguardar que logo o café chegará até você</p>
+        </div>
+        <SuccessInfo>
+          <SuccessItem>
+            <div className="item__icon" style={{ background: '#8047F8' }}>
+              <MapPin weight="fill" />
+            </div>
+            <div>
+              <p>
+                Entrega em{' '}
+                <span>
+                  {address.street + ', ' + address.number + address.complement}
+                </span>
+              </p>
+              <p>Centro - Valença, RJ</p>
+            </div>
+          </SuccessItem>
+          <SuccessItem>
+            <div className="item__icon" style={{ background: '#DBAC2C' }}>
+              <Timer weight="fill" />
+            </div>
+            <div>
+              <p>Previsão de entrega</p>
+              <span>20 min - 30 min</span>
+            </div>
+          </SuccessItem>
+          <SuccessItem>
+            <div className="item__icon" style={{ background: '#C47F17' }}>
+              <CurrencyDollar weight="fill" />
+            </div>
+            <div>
+              <p>Pagamento na entrega</p>
+              <span>{paymentMethodTranslate}</span>
+            </div>
+          </SuccessItem>
+        </SuccessInfo>
+      </div>
+      <img src={deliveryIllustration} alt="" />
+    </SuccessContainer>
+  )
 }
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -1,3 +1,70 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-export const SuccessContainer = styled.main``
+export const SuccessContainer = styled.main`
+  max-width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  > div {
+    .success__title {
+      > h1 {
+        font-family: 'Baloo 2';
+        font-size: 2rem;
+        font-weight: 800;
+        color: ${(props) => props.theme['yellow-dark']};
+      }
+
+      > p {
+        font-size: 1.25rem;
+        color: ${(props) => props.theme['base-subtitle']};
+      }
+    }
+  }
+
+  img {
+    width: auto;
+    max-width: 30.75rem;
+    height: auto;
+    max-height: 18rem;
+  }
+`
+
+export const SuccessInfo = styled.div`
+  padding: 2.5rem;
+  background: ${(props) => props.theme.white};
+  border-radius: 0.375rem 2.25rem;
+  position: relative;
+  margin-top: 3rem;
+
+  &::before {
+    content: '';
+    z-index: -1;
+    background: linear-gradient(to right, #dbac2c, #8047f8);
+    border-radius: 7px 37px;
+    position: absolute;
+    inset: -1px;
+  }
+`
+
+export const SuccessItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &:not(:first-child) {
+    margin-top: 2rem;
+  }
+
+  .item__icon {
+    color: white;
+    padding: 0.5rem;
+    border-radius: 50%;
+    line-height: 0;
+  }
+
+  span {
+    font-weight: 700;
+  }
+`
